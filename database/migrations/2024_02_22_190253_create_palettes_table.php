327e4cf85ca1e985352681d1e86e511c5f1a36c6
Debora MR <dmedina@iesrafaelalberti.es>
2024-02-22 20:21:45 +0100

    Configurando Migrations

--- database/migrations/2024_02_22_190253_create_palettes_table.php
+++ database/migrations/2024_02_22_190253_create_palettes_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('palettes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
-            $table->integer('user_id');
+            $table->integer('project_id');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('palettes');
     }
 };
--- database/migrations/2024_02_22_191823_create_palette_color_table.php
+++ database/migrations/2024_02_22_191823_create_palette_color_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('palette_color', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->integer('user_id');
+            $table->integer('palette_id');
+            $table->integer('color_id');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('palette_color');
     }
 };
